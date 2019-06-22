847676ca7259f6fc4e08fedad2222785da5e972f
Jack Maldonado <jackmaldonado@Jacks-MacBook-Pro.local>
2019-06-22 15:27:11 -0700

    moved functionality to square

--- src/Board.js
+++ src/Board.js
@@ -1,27 +1,28 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Square from './Square';
 // import Leaf from './leaf.JPEG';
 
 
 
 class Board extends Component {
-    constructor(props){
+    constructor(props) {
         //console.log("Board::constructor")
         super(props)
         this.state = {
-            question: ['?', '?', '?', '?', '?', '?','?','?', '?'], //The visible top layer of "?"
-            content: ["tree", "tree", "tree","tree","tree","tree","tree","tree","tree"], //The content that is really there but hidden
+            question: ['?', '?', '?', '?', '?', '?', '?', '?', '?'], //The visible top layer of "?"
+            content: ["tree", "tree", "tree", "tree", "tree", "tree", "tree", "tree", "tree"], //The content that is really there but hidden
             tries: 0,
             maxTries: 5,
             winOrLose: "" //Has three values of "win","lose" or ""
         }
 
         // created a var for a random index to contain a coin
-        let target = Math.floor(Math.random()*9)
+        let target = Math.floor(Math.random() * 9)
         // Turns the random index into a coin and places it
         this.state.content[target] = "coin"
-    } 
-    
+    }
+
     updateImage = (e) => {
 
         // take data and created variables to modify the state
@@ -29,69 +30,49 @@ class Board extends Component {
         let { tries } = this.state
         let { maxTries } = this.state
         let { winOrLose } = this.state
-        
+
         // create a var to access the index and value of the target square
         let target = e.target.id
         let content = this.state.content[target]
 
-        if (question[target] === "?"){ // the square has not been clicked on
+        if (question[target] === "?") { // the square has not been clicked on
             question[target] = this.state.content[target] // after clicked, the "?" turns into the content
             maxTries -= 1 // This decreases the amount of tries after each click
             // set winOrLose
-            if (content === "coin"){
+            if (content === "coin") {
                 winOrLose = "win"
                 alert("You Win!")
                 //window.location.reload() 
-            } else if(tries === maxTries){
+            } else if (tries === maxTries) {
                 alert("You Lose!")
                 winOrLose = "lose"
             }
-            
-        } 
 
-        this.setState({question, tries, maxTries, winOrLose})
+        }
+
+        this.setState({ question, tries, maxTries, winOrLose })
     }
-    
 
-    render(){
-        const coin = <img src="./coin.jpeg"/>
+
+    render() {
+        const coin = <img src="./coin.jpeg" />
         const { question } = this.state
         let { winOrLose } = this.state
-        
+
         //  loops over all 9 squares and returns the html content
         let squares = question.map((value, index) => {
-            if (value === "coin"){ // it displays the image of a coin
-                return(
-                    <div><img src="./coin.jpeg"/></div>
-                )
-            }else if(value === "tree"){ //has been clicked but was not the right spot
-                if (winOrLose === ""){
-                    return(
-                       <div><img src="./lebron.jpeg"/></div>
-                    )
-                }else{
-                    return (
-                        <div>{winOrLose}</div>
-                    )
-                }
-            }else{ // has not been clicked on
-                if(winOrLose === ""){
-                    return (
-                         <div id={index} onClick={this.updateImage}>{value}</div>
-                    )                    
-                }else{
-                    return (
-                        <div>{winOrLose}</div>
-                    )
-                }
-            }
+
+            return (
+                <Square value={value} winOrLose={winOrLose} index={index } callback={this.updateImage}/>
+            )
+
         })
         // let gameState say "win" or "lose" based on this.state.winOrLose
         return (
             <div className="board">
                 {squares}
                 <h1>
-                     Tries Left: {this.state.maxTries}  
+                    Tries Left: {this.state.maxTries}
                 </h1>
             </div>
         );
@@ -122,4 +103,4 @@ export default Board;
     //         <div id={question[6]} onClick={this.updateImage}>{this.state.question[6]}</div>
     //         <div id={question[7]} onClick={this.updateImage}>{this.state.question[7]}</div>
     //         <div id={question[8]} onClick={this.updateImage}>{this.state.question[8]}</div>
-   
+
--- src/Square.js
+++ src/Square.js
@@ -1,21 +1,45 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 class Square extends Component {
-    constructor(props){
-        super(props)
-            this.state = {
-                spaces: ['tree', 'tree', 'tree', 'tree', 'tree', 'tree', 'tree', 'tree', 'tree']
-            }
+  
+    onClick = (e) => {
+        alert("square clicked")
     }
-    render(){
-      return (
-              <div>
-
-            </div>
-        )
+    render() {
+        let value = this.props.value
+        let winOrLose = this.props.winOrLose
+        let index = this.props.index
         
-  }
+        if (value === "coin") { // it displays the image of a coin
+            return (
+                <div><img src="./coin.jpeg" /></div>
+            )
+        } else if (value === "tree") { //has been clicked but was not the right spot
+            if (winOrLose === "") {
+                return (
+                    <div><img src="./lebron.jpeg" /></div>
+                )
+            } else {
+                return (
+                    <div>{winOrLose}</div>
+                )
+            }
+        } else { // has not been clicked on
+            if (winOrLose === "") {
+                return (
+                    <div id={index} onClick={this.props.callback}>{value}</div>
+                )
+            } else {
+                return (
+                    <div>{winOrLose}</div>
+                )
+            }
+        }
+     
+      
+
+    }
 }
 
 export default Square;
